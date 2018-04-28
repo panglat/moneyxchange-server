16215855e5245ad48e720c6f9f8ded68be2ec2a5
Pablo Anglat <panglat@gmail.com>
2018-04-28 18:55:33 -0300

    Enable logging

--- MoneyXChangeServer/Controllers/ValuesController.cs
+++ MoneyXChangeServer/Controllers/ValuesController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoneyXChangeServer.Core;
 
 namespace MoneyXChangeServer.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this._logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            _logger.LogInformation(LoggingEvents.ListItems, "Getting items");
             return new string[] { "value1", "value2" };
         }
 
@@ -20,6 +30,7 @@ namespace MoneyXChangeServer.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
+            _logger.LogInformation(LoggingEvents.GetItem, "Getting item {ID}", id);
             return "value";
         }
 
